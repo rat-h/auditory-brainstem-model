cc86d48fb4f982c9b001fec73c52c81b07d26ea4
Ruben A. Tikidji-Hamburyan <rth@nisms.krinc.ru>
2014-08-11 12:55:07 -0500

    add sleep 1 to be sure that libresumple is available

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ case $1 in
 	clean)
 		pushd tools
 		rm -fR ZilanyCarney2009AN.so ffGn.py audiotools.so ZilanyCarney-JASAcod-2009/build audiotools/build lib* *.pyc
-		pushd audiotools/libresample-0.1.3; [ -e Makefile ] && make clean; popd
+		pushd audiotools/libresample-0.1.3; [ -e Makefile ] && make dist; popd
 		popd
 		pushd ../tools
 		rm -fR ZilanyCarney2009AN.so ffGn.py audiotools.so
@@ -18,8 +18,11 @@ case $1 in
 		pushd tools &&
 			pushd ZilanyCarney-JASAcod-2009 && python setup.py install --prefix=../ && popd && 
 			pushd audiotools && 
-				pushd libresample-0.1.3 && ( [ -e Makefile ] && make dist ) || echo "CLEAR"  && ./configure && make && popd &&
-				python setup.py install --prefix=../ && popd && 
+				pushd libresample-0.1.3 && 
+					( [ -e Makefile ] && make dist );  ./configure && make && sleep 1 &&
+				popd &&
+				python setup.py install --prefix=../ && 
+			popd && 
 			for i in $(find lib* -name "*.so"); do ln -s $i; done
 			for i in $(find lib* -name "*.py"); do ln -s $i; done
 		popd
